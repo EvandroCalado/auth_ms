f231f3991e99a37104fe0ea6302cebc5e3e55324
Evandro Calado <evandrocalado07@gmail.com>
2024-12-25 17:58:50 -0300

    feat: enhance authentication controller with user registration and login DTOs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,21 @@
 import { Controller } from '@nestjs/common';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { LoginUserDto } from './dto/login-user.dto';
+import { RegisterUserDto } from './dto/register-user.dto';
 
 @Controller()
 export class AuthController {
   constructor() {}
 
   @MessagePattern('auth.register.user')
-  registerUser() {
-    return 'register user';
+  registerUser(@Payload() registerUserDto: RegisterUserDto) {
+    return registerUserDto;
   }
 
   @MessagePattern('auth.login.user')
-  loginUser() {
-    return 'login user';
+  loginUser(@Payload() loginUserDto: LoginUserDto) {
+    console.log(loginUserDto);
+    return loginUserDto;
   }
 
   @MessagePattern('auth.verify.user')
